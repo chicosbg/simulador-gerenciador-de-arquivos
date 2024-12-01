7abce7176604e2fef566218ad104ff860c05f411
Luis Gomes <luisfrancisco.15.brum@gmail.com>
2024-12-01 01:13:42 -0300

    logica diretorios

--- src/modulos/display/display.py
+++ src/modulos/display/display.py
@@ -4,7 +4,7 @@ from tkinter import ttk
 
 class Display(): 
     def __init__(self) -> None:
-        self.root = Tk(screenName="Simulador", className="Simulador")
+        self.root = Tk(screenName="Simulador", className="Simulador de gerenciamento de arquivos")
         self.root.geometry("600x400")
     
     def set_text(self, text, col: int, row: int = 0, handler = None):    
@@ -14,7 +14,8 @@ class Display():
         if handler:
             label.configure(cursor="hand2")
             label.bind("<Button-1>", handler)
-
+        return label
+    
     def set_frame(self, w, c=0, r=0, border: str|None = None):
         fram = ttk.Frame(self.root, padding=10)
         fram.grid(column=c, row=r)
@@ -29,9 +30,12 @@ class Display():
         
 def abre_diretorio(display: Display, column = 1, row = 0):
     display.set_frame(60, column, row, "groove")
-    display.set_text("teste", col=column+1, handler=lambda x: abre_diretorio(display=display, column=column+2, row=7))
+    display.set_text(f"sub_diretorio{column-1}", col=column+1, row=row, handler=lambda x: abre_diretorio(display=display, column=column+2, row=row))
+
 
 if __name__ == "__main__":
     d = Display()
-    d.set_text("teste", 0, handler=lambda x: abre_diretorio(d))
+    a = [0,1,2,3,4,5]
+    for i in a:
+        print(d.set_text(f"diretorio_{i}", 0, row=i, handler=lambda event, i=i: abre_diretorio(d, row=i)))
     d.run()    
