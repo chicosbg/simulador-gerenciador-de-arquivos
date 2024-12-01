b44a286ea2eade735e6a4f1ffd6cf42a05b34466
Luis Gomes <luisfrancisco.15.brum@gmail.com>
2024-12-01 01:43:02 -0300

    add encadeada

--- src/modulos/models/arquivos.py
+++ src/modulos/models/arquivos.py
@@ -1,5 +1,9 @@
+"""
+    justificativa da encadeada: achei legal ;)
+"""
 class Arquivo():
     def __init__(self) -> None:
+        self.blocos = [{"id": 0, "posicao_alocado_disco": 0, "posicao_proximo_disco": 0}, {"id":2, "posicao_alocado_disco": 0, "posicao_proximo_disco": 0}]
         self.nome = "??"
         self.atributos = {"??":"??"}
         self.tipo = "??"
