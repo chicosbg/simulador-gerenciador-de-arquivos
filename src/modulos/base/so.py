8a11664c54f1f56467b3b3fdb01ccab516de80c4
Y4ngfr <148384398+Y4ngfr@users.noreply.github.com>
2024-12-03 14:56:27 -0300

    Add files via upload

--- src/modulos/base/so.py
+++ src/modulos/base/so.py
@@ -3,19 +3,19 @@ from modulos.operacoes_cmd.trata_operacoes_cmd import TratadorComandosCMD
 class SO():
     def __init__(self):
         self.diretorios_atual = None
-        self.trador_comandos = TratadorComandosCMD()
+        self.tratador_comandos = TratadorComandosCMD()
         
     def comando_cmd(self, comando: str):
         comando = comando.split(' ')
         if(comando[0].lower() == 'cd'):
-            dir_atual = self.trador_comandos.cd(comando, self.diretorios_atual)   
+            dir_atual = self.tratador_comandos.cd(comando, self.diretorios_atual)   
             self.diretorios_atual = dir_atual
         
         if(comando[0].lower() == 'ls'):
-            dirs = self.trador_comandos.ls(comando, self.diretorios_atual)    
+            dirs = self.tratador_comandos.ls(comando, self.diretorios_atual)    
             for diretorio in dirs:
                 print(diretorio.nome) 
             
         if(comando[0].lower() == 'pwd'):
-            print(self.trador_comandos.pwd(comando, self.diretorios_atual))    
+            print(self.tratador_comandos.pwd(comando, self.diretorios_atual))    
              
